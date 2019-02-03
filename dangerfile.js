653a44b44fcece20a9b1b2dcb327d4cee3c12836
Issei Horie <issei.horie@nulab-inc.com>
2019-02-03 22:40:46 +0900

    Updated

--- dangerfile.js
+++ dangerfile.js
@@ -3,5 +3,7 @@ import {message} from "danger";
 const typetalk = require("danger-plugin-typetalk").default;
 
 message("Hello, World!");
+warn("You have not included a CHANGELOG entry.")
+fail(`ESLint has failed.`)
 
 typetalk();
